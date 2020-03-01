8a08a3cd17128026a031276480561166b6d881ec
github username <github email address>
2020-03-01 18:28:04 -0500

    added lines and road

--- index.js
+++ index.js
@@ -3,39 +3,55 @@ const startScreen = document.querySelector(".startScreen")
 const gameArea = document.querySelector(".gameArea")
 
 let keys = { 
-    ArrowUp:false, ArrowDown:false, ArrowRight:false, ArrowLeft:false
+    ArrowUp: false, 
+    ArrowDown: false, 
+    ArrowRight: false, 
+    ArrowLeft: false
 }
 
 let player = {speed:5}
 
-const playGame = (e) => {
+function playGame(e){
     let car = document.querySelector(".car")
+    moveLines()
+    let road = gameArea.getBoundingClientRect()
+    
     if(player.start){
-        if(keys.ArrowUp){
+        
+        if(keys.ArrowUp && player.y < (road.height -150)){
             player.y -= player.speed
         }
-        if(keys.ArrowDown){
+        if(keys.ArrowDown && player.y < road.bottom){
             player.y += player.speed
         }
-        if(keys.ArrowLeft){
+        if(keys.ArrowLeft && player.x > 0){
             player.x -= player.speed
         }
-        if(keys.ArrowRight){
+        if(keys.ArrowRight && player.x < (road.width - 50)){
             player.x += player.speed
         }
+        
         car.style.left = `${player.x}px`
         car.style.top = `${player.y}px`
 
         window.requestAnimationFrame(playGame)
     }
-    console.log("in play")
     
 }
 
-const start = () => {
+function start(){
     startScreen.classList.add("hide")
     gameArea.classList.remove("hide")
+    
     player.start = true
+    for(let i=0; i < 10; i++){
+        let line = document.createElement("div")
+        line.classList.add("line")
+        line.y = i* 150
+        line.style.top = (`${i* 150}px`)
+        gameArea.appendChild(line)
+    } 
+    
     window.requestAnimationFrame(playGame)
     
     let car = document.createElement("div")
@@ -44,9 +60,9 @@ const start = () => {
     car.setAttribute("class","car")
     
     gameArea.appendChild(car)
+    
     player.x =car.offsetLeft
     player.y =car.offsetTop
-    console.log(player)
 
 }
 
@@ -61,7 +77,16 @@ const pressOff = (e) => {
 }
 
 
-
+const moveLines = () => {
+    let lines = document.querySelectorAll(".line")
+    lines.forEach((item) => {
+        if(item.y > 1500){
+            item.y -= 1500
+        }
+        item.y += player.speed
+        item.style.top = `${item.y}px`
+    })
+}
 startScreen.addEventListener("click", start)
 document.addEventListener("keydown", pressOn)
 document.addEventListener("keyup", pressOff)
